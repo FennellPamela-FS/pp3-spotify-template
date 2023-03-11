abb00f125bb7bbb9746ac38adcdd2265a8b6fec1
FennellPamela-FS <pyfennell@student.fullsail.edu>
2023-03-11 18:06:55 -0500

    Setup music router

--- api/routes/music.js
+++ api/routes/music.js
@@ -1,5 +1,30 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express'); // express server
+const router = express.Router();    // express class to handle routes
+
+// use routes to access http verbs( GET, POST, PUT, DELETE)
+// route handler has two parts - url/path and callback function
+// router.get('/', (req, res) => {
+
+// /handle incoming GET requests    
+router.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Hello World! Incoming GET request.',
+    })
+});
+
+// handle POST requests express.Router() class
+router.post('/', (req, res) => {
+    res.status(200).json({
+        message: 'Hello World! POST request.',
+    })
+});
+
+// handle POST requests by ID
+router.post('/:id', (req, res) => {
+    res.status(200).json({
+        message: 'Hello World! POST request by ID.',
+    })
+});
 
 
 module.exports = router;
